c70047ec3c8472adda9a94b0a33049df041d7c8b
LViniciusk <botlink2030@gmail.com>
2022-04-10 17:23:39 -0300

    Update index.php

--- ex001/index.php
+++ ex001/index.php
@@ -5,11 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        h1{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h2{
+            font-family: cursive;
+            color: lightblue;
+        }
+
+
+    </style>
 </head>
 <body>
     <h1>Testando PHP</h1>
     <?php
-        echo "Hello, World!";
+        echo "<h2>Hello, World!</h2>";
     ?>
     
 </body>
