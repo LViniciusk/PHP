9c6a5b66b98f06c91291e61deaf01c9f5e301823
LViniciusk <botlink2030@gmail.com>
2022-04-10 17:43:29 -0300

    .

---
+++ exercicios/ex002/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex002</title>
+</head>
+<body>
+    <h1>Testando Variaveis</h1>
+    <?php
+        $n1 = 3;
+        $n2 = 7;
+        $result = $n1 + $n2;
+        echo "<p> n1 = $n1 <br> n2 = $n2<br> result = $n1 + $n2 <br> result = $result</p>";
+    ?>
+</body>
+</html>
